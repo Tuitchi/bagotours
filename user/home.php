e6c1439968bba5905df7601dda28d4860dc3c496
Fjondaniel <158798598+Fjondaniel@users.noreply.github.com>
2024-09-06 23:37:18 +0800

    Update home.php
    asasdasd

--- user/home.php
+++ user/home.php
@@ -12,44 +12,50 @@ $popularTours = getAllPopular($conn);
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
   <link rel="stylesheet" href="assets/css/style.css">
  </head>
+ <?php include('inc/topnav.php'); ?>
 <body>
+<main id="main">
 
-  <?php include('inc/topnav.php'); ?>
-  <main id="main">
+  
     <div class="content">
-      <header class="recomendation">
-        <?php
-        $tab = 1;
-        foreach ($tours as $tour) { ?>
-          <div id="tab<?php echo $tab; ?>" class="tab <?php echo $tab === 1 ? 'active' : ''; ?>">
-            <div class="tabcontainer">
-              <div class="details">
-                <h2><?php echo $tour['title'] ?></h2>
-                <p><?php echo $tour['description'] ?></p>
-              </div>
-              <div>
-                <img src="../upload/Tour Images/<?php echo $tour['img'] ?>" alt="">
-              </div>
-            </div>
-          </div>
-        <?php
-          $tab++;
-        }
-        ?>
-        <div class="circle-container">
-          <?php for ($i = 1; $i < $tab; $i++) { ?>
-            <button class="circle-button" onclick="showTab(<?php echo $i; ?>)"></button>
-          <?php } ?>
+    <header class="recomendation">
+  <?php
+  $tab = 1;
+  foreach ($tours as $tour) { ?>
+    <div id="tab<?php echo $tab; ?>" class="tab <?php echo $tab === 1 ? 'active' : ''; ?>">
+      <div class="tabcontainer">
+        <div class="details">
+          <h2><?php echo $tour['title'] ?></h2>
+          <p><?php echo $tour['description'] ?></p>
         </div>
-      </header>
+        <div>
+          <img src="../upload/Tour Images/<?php echo $tour['img'] ?>" alt="">
+        </div>
+      </div>
+    </div>
+  <?php
+    $tab++;
+  }
+  ?>
+  <div class="circle-container">
+    <?php for ($i = 1; $i < $tab; $i++) { ?>
+      <button class="circle-button" onclick="showTab(<?php echo $i; ?>)"></button>
+    <?php } ?>
+  </div>
+</header>
+
+
       <!-- Tours Section -->
-      <div class="head">Tours</div>
+      
       
 
 
       <aside class="cardmain">
+      <div class="head">Tours</div>
         <?php
         foreach ($tours as $tour) {
           $average_rating = getAverageRating($conn, $tour['id']); ?>
@@ -71,25 +77,6 @@ $popularTours = getAllPopular($conn);
     </div>
    
 
-<<<<<<< Updated upstream
-          <div class="destination">
-            <a class="popLink" href="tour?tours=<?php echo $popTour['id'] ?>">
-              <div class="Popcontent">
-                <img src="../upload/Tour Images/<?php echo $popTour['img'] ?>" alt="">
-                <div class="details">
-                  <h2><?php echo $popTour['title'] ?></h2>
-                  <p><?php echo $popTour['type'] ?>
-                    <h7 class="rating">⭐<?php echo number_format($average_rating, 1) ?></h7>
-                  </p>
-                </div>
-              </div>
-            </a>
-          </div>
-        <?php } ?>
-      </div>
-=======
-
->>>>>>> Stashed changes
   </main>
   <?php include ('inc/footer.php') ?>
 
